ca70395be01ef2b925054cfa4575518e9b573cee
Gianina Azcurra <99845373+gianinazcurra@users.noreply.github.com>
2025-10-24 11:23:01 -0300

    Add files via upload
    ESTE ES EL MAIN CORRECTO, EL QUE INCLUYE EL ARRAYLIST :d

--- Ejercicio2_Main.java
+++ Ejercicio2_Main.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package programacionii_semanavii;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -12,11 +11,24 @@ public class Ejercicio2_Main {
     
     public static void main(String[] args) {
         
-        Figura figura1 = new Circulo(2.5,"círculo");
-        Figura figura2 = new Rectangulo (5.5,4,"rectángulo");
+        //Array vacío de figuras
+        ArrayList<Figura> figuras = new ArrayList<>();
         
-        figura1.calcularArea();
-        figura2.calcularArea();
+        //Crep e instancio figuras distinas
+        Rectangulo rectangulo1 = new Rectangulo (5.5,4,"rectángulo");
+        Rectangulo rectangulo2 = new Rectangulo (3.6,6,"rectángulo");
+        Circulo circulo1 = new Circulo (5.5,"círculo");
+        Circulo circulo2 = new Circulo (2,"círculo");
         
-    }
+        //Las agrego al array
+        figuras.add(rectangulo1);
+        figuras.add(rectangulo2);
+        figuras.add(circulo1);
+        figuras.add(circulo2);
+        
+        //recorro la colección para calcular el área de cada una de ellas
+        for(Figura f : figuras){
+        f.calcularArea();
 }
+}
+    }
